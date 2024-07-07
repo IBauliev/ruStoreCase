4bb9921a7beae12dc09ae5869f5e628b8e001862
Kr0uxx <m7a7x7gg@gmail.com>
2024-07-07 05:32:56 +0300

    99.5% works

--- server.py
+++ server.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    answer = ''
     if request.method == 'POST':
         user_input = request.form['user_input']
         answer = give_response(user_input)
         print(answer)
-        return render_template('index.html', answer=answer)
-    return render_template('index.html')
+    return render_template('index.html', answer=answer)
 
 
 if __name__ == '__main__':
